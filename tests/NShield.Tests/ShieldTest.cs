7de30c473f8cb6b5012152f2f934b6be61f89936
Carlos Schults <carlos.schults@gmail.com>
2019-04-23 12:32:31 -0300

    refactor 3 tests into 1 by using parameterized values

--- tests/NShield.Tests/ShieldTest.cs
+++ tests/NShield.Tests/ShieldTest.cs
@@ -6,27 +6,13 @@ namespace NShield.Tests
 {
     public class Tests
     {
-        [TestCase]
-        public void Test1()
+        [TestCase("doesn't look like an empty string!")]
+        [TestCase(" ")]
+        [TestCase("\n\n\r")]
+        public void AgainstEmptyString_WithNonEmptyString_ShouldReturnTheStringItself(string value)
         {
-            var expected = "doesn't look like an empty string!";
-            string result = Shield.AgainstEmptyString("doesn't look like an empty string!");
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestCase]
-        public void Test2()
-        {
-            var expected = " ";
-            string result = Shield.AgainstEmptyString(" ");
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestCase]
-        public void Test3()
-        {
-            var expected = "\n\n\r";
-            string result = Shield.AgainstEmptyString("\n\n\r");
+            string expected = value;
+            string result = Shield.AgainstEmptyString(value);
             Assert.AreEqual(expected, result);
         }
 
